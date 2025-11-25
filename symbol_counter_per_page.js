d5895e33867d2ae293ccc0a941f5c364a664aba2
dzhanomkontselidze <dzhano.m.kontselidze@ukd.edu.ua>
2025-11-25 19:04:31 +0200

    Create symbol_counter_per_page.js
    used to pages and symbols for information.txt

---
+++ symbol_counter_per_page.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const SYMBOLS_COUNTING_PER_PAGE = 80 * 20;
+
+const data = fs.readFileSync(
+    'information.txt',
+    {
+        encoding: 'utf8',
+        flag: 'r'
+    }
+);
+
+function countPages(symbolsInBook, symbolsOnPage){
+    if(typeof symbolsInBook !== `number` || typeof symbolsOnPage !== `number`){
+        return 0
+    }
+            
+    if(symbolsOnPage == 0 || symbolsOnPage == null){
+        return 0  
+    }
+
+    return Math.ceil(symbolsInBook/Math.abs(symbolsOnPage))    
+}
+
+function getPage(book, pageNumber) {
+    console.log(book.slice(SYMBOLS_COUNTING_PER_PAGE * (pageNumber - 1), SYMBOLS_COUNTING_PER_PAGE * pageNumber))
+//    return // page text
+}
+
+
+const pagesCount = countPages(data.length, SYMBOLS_COUNTING_PER_PAGE)
+console.log(pagesCount);
+const pageText = getPage(data, 1);
+console.log(pageText)
